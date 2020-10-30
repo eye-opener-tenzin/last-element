f1a2ef46a7f87adc4522e15f5e47db1eafc4c301
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-29 20:18:16 -0700

    add last element exercise file to git

---
+++ last-element.js
@@ -0,0 +1,9 @@
+// DEFINE YOUR FUNCTION BELOW:
+
+const lastElement = arr => arr.length ? console.log(arr[arr.length-1]) :  console.log(null)
+
+
+lastElement([3, 5, 7]);
+lastElement([]);
+lastElement([1])
+lastElement([1, 2, 3])
